29a6896709770493d70ecc6366650a53271f58a8
Guilherme Euller Dantas Silva <guilhermeeuller10@gmail.com>
2024-03-15 08:47:41 -0300

    adição da logo do restaurante e da font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MaterialApp(title: "Contador de pessoas", home: Home()));
@@ -14,6 +15,7 @@ class Home extends StatefulWidget {
 class HomeState extends State<Home> {
   int _pessoa = 0;
   String _mensagem = "Pode entrar!";
+  double _tamanho = 0.0;
 
   void _changePeople(int delta) {
     setState(() {
@@ -21,10 +23,13 @@ class HomeState extends State<Home> {
       if (_pessoa >= 20) {
         _mensagem = "Lotado, não pode entrar.";
         _pessoa = 20;
+        _tamanho = _pessoa * 10;
       } else if (_pessoa >= 0) {
         _mensagem = "Pode Entrar!";
+        _tamanho = 60 + _pessoa * 3;
       } else {
         _pessoa = 0;
+        _tamanho = 60 + _pessoa * 3;
       }
     });
   }
@@ -42,19 +47,41 @@ class HomeState extends State<Home> {
                 child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text("$_pessoa",
-                    style: const TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold))
+                Image.asset("imagens/logo_restaurante.png",
+                    fit: BoxFit.cover, height: 100),
+                const Text("Restaurante Mexicano",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        decoration: TextDecoration.none)),
+                Expanded(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("$_pessoa",
+                        style: GoogleFonts.bangers(
+                            textStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: _tamanho,
+                                fontWeight: FontWeight.bold,
+                                decoration: TextDecoration.none)))
+                  ],
+                ))
               ],
             )),
             Center(
-                child: Text(
-              _mensagem,
-              style: const TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30),
-            )),
+                child: Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      _mensagem,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 25,
+                        decoration: TextDecoration.none,
+                      ),
+                    ))),
             Expanded(
               child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
